9651250615524bd273d66b1a541ca1ac38a817df
Jack Hall <52833622+johnragonhall@users.noreply.github.com>
2026-06-14 02:28:47 -0400

    fix(config): remove unused imports and exclude tests from tsconfig
    COMPONENTS
    - Unused imports removed from ShapeBar
    
    CONFIG
    - Root tsconfig updated to exclude __tests__ directory

--- mobile/src/components/ShapeBar.tsx
+++ mobile/src/components/ShapeBar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import type { Shot, ShotShape } from '../types/shot';
-import { classifyShotShape, shotShapeColor } from '../utils/shotShape';
+import { classifyShotShape } from '../utils/shotShape';
 import { C, R } from '../theme';
 
 interface Props {
